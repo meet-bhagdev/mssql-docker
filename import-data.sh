1b1b62a2dddd301cfebb869a652b9be6071e26ee
Meet Bhagdev <mebha@microsoft.com>
2017-04-05 21:28:36 -0700

    removed some files

--- import-data.sh
+++ import-data.sh
@@ -2,7 +2,7 @@
 sleep 90s
 
 #run the setup script to create the DB and the schema in the DB
-/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P Yukon900 -d master -i setup.sql
+/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P Yukon900 -d master -i /usr/src/app/setup.sql
 
 #import the data from teh csv file
 /opt/mssql-tools/bin/bcp DemoData.dbo.Products in "/usr/src/app/Products.csv" -c -t',' -S localhost -U sa -P Yukon900
